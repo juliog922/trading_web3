391441bf9aaf218b6a27e634a7e6d6d3afeeeafe
Julio Galindo <134719853+juliog922@users.noreply.github.com>
2024-05-24 16:27:57 +0200

    Delete frontend/target directory

--- frontend/target/debug/deps/frontend-514b16db6dc58c82.d
+++
@@ -1,5 +0,0 @@
-C:\Users\julio\OneDrive\Escritorio\Portfolio\trading_web3\frontend\target\debug\deps\libfrontend-514b16db6dc58c82.rmeta: src\main.rs
-
-C:\Users\julio\OneDrive\Escritorio\Portfolio\trading_web3\frontend\target\debug\deps\frontend-514b16db6dc58c82.d: src\main.rs
-
-src\main.rs:
--- frontend/target/debug/deps/frontend-b50cbcdb921ae988.d
+++
@@ -1,5 +0,0 @@
-C:\Users\julio\OneDrive\Escritorio\Portfolio\trading_web3\frontend\target\debug\deps\libfrontend-b50cbcdb921ae988.rmeta: src\main.rs
-
-C:\Users\julio\OneDrive\Escritorio\Portfolio\trading_web3\frontend\target\debug\deps\frontend-b50cbcdb921ae988.d: src\main.rs
-
-src\main.rs:
